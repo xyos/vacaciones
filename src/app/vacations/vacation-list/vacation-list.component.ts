3f78a60aaf4394b4ae17c4e24703d09395319c16
Jairo Suarez <xyos@192.168.133.128>
2018-03-04 16:36:38 -0800

    adding simple mock service

--- src/app/vacations/vacation-list/vacation-list.component.ts
+++ src/app/vacations/vacation-list/vacation-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { VacationsService } from '../shared/vacations.service';
+import { VacationRequest } from '../shared/vacation-request.model';
 
 @Component({
   selector: 'app-vacation-list',
@@ -6,10 +8,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./vacation-list.component.css']
 })
 export class VacationListComponent implements OnInit {
+  requests: VacationRequest[];
 
-  constructor() { }
+  constructor(private vacationsService: VacationsService) { }
 
   ngOnInit() {
+    console.log(this.vacationsService)
+    this.getRequests();
+  }
+
+  getRequests() : void { 
+    this.vacationsService.getRequests()
+    .subscribe(requests => this.requests = requests);
   }
 
 }
--- src/app/vacations/vacations.module.ts
+++ src/app/vacations/vacations.module.ts
@@ -4,12 +4,16 @@ import { VacationListComponent } from './vacation-list/vacation-list.component';
 import { VacationDetailComponent } from './vacation-detail/vacation-detail.component';
 import { VacationsComponent } from './vacations.component';
 import { VacationListItemComponent } from './vacation-list/vacation-list-item/vacation-list-item.component';
+import { VacationsService } from './shared/vacations.service';
 
 @NgModule({
   imports: [
     CommonModule
   ],
   declarations: [VacationListComponent, VacationDetailComponent, VacationsComponent, VacationListItemComponent],
+  providers: [
+    VacationsService,
+  ],
   exports: [
     VacationsComponent,
   ]
