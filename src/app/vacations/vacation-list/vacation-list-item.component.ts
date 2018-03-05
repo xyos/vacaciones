4f1aed364c5b3468f2608ca2ece517ec70f28b93
Jairo Suarez <jasuarezpr@gmail.com>
2018-03-04 20:53:38 -0800

    show list

--- src/app/vacations/vacation-list/vacation-list-item.component.ts
+++ src/app/vacations/vacation-list/vacation-list-item.component.ts
@@ -1,11 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { VacationRequest } from '../shared/vacation-request.model';
 
 @Component({
   selector: 'vacation-list-item',
   templateUrl: './vacation-list-item.component.html',
   styleUrls: ['./vacation-list-item.component.css']
 })
+
 export class VacationListItemComponent implements OnInit {
+  @Input() request: VacationRequest;
 
   constructor() { }
 
